058888e1128bc7147ade6a3c741c7e445d6a75c2
Nahum Casco <nahumcasco.v@gmail.com>
2022-12-28 21:22:00 -0500

    set href and items number

--- resources/views/components/warehouseCard.blade.php
+++ resources/views/components/warehouseCard.blade.php
@@ -1,16 +1,16 @@
 <div class="warehouse_card">
-    <a href="#">
+    <a href="{{route('warehouse.items', $data->id)}}">
         <div class="wcard_bg_title">
             <i class="fa-solid fa-warehouse"></i> 
             <span>Almacen</span>
         </div>
         <div class="wcard_body">
             <div class="wcard_body_title">
-                <p class="wcard_title">Warehouse calle sur asda sdasdas</p>
+                <p class="wcard_title">{{$data->name}}</p>
             </div>
             <div class="wcard_body_items">
                 <i class="fa-solid fa-box-archive"></i>
-                items: 10
+                items: {{$data->items}}
             </div>
         </div>
     </a>
